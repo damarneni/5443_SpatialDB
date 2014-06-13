9edc087f7ee6344fe7d7e732a0da1799af2cb67d
damarneni <damarneni@cs2.mwsu.edu>
2014-06-13 12:33:45 -0500

    Adding PolygonOverlay

--- PolygonOverlay/functions.php
+++ PolygonOverlay/functions.php
@@ -4,11 +4,13 @@ function sql_to_coordinates($blob)
         $blob = str_replace("))", "", str_replace("POLYGON((", "", $blob));
         $coords = explode(",", $blob);
         $coordinates = array();
+		//for($a = 0;$a <sizeof($coords);$a++){
         foreach($coords as $coord)
         {
             $coord_split = explode(" ", $coord);
             $coordinates[]=array("lat"=>$coord_split[0], "lng"=>$coord_split[1]);
         }
+		//}
         return $coordinates;
     }
 
--- PolygonOverlay/index.php
+++ PolygonOverlay/index.php
@@ -5,19 +5,30 @@
 	unset($Coords);
 		if(isset($_POST['Countries'])){
         $id = $_POST['Countries'];
-		$size = sizeof($id);
+		//$size = sizeof($id);
 		/* echo "<pre>";
 		print_r($id);
 		echo "</pre>"; */
-		$id = implode($id,",");
+		//$id = implode($id,",");
+		$j = 0;
+		foreach($id as $i){
 		$result = $con->query("SELECT asText(SHAPE) as border
                                FROM `world_borders` 
-                               WHERE CountryID = '{$id}'");
-
-            $Result = $result->fetch_assoc();
-
+                               WHERE CountryID = '{$i}'");
+		
+		print_r($result);
+		echo "<br>";
+		echo "<br>";
+			$Result = $result->fetch_assoc();
+			//print_r($Result);
             $Coords = sql_to_coordinates($Result['border']);
-			//print_r($Coords);
+			print_r($Coords);
+			echo "<br>";
+			echo "<br>";
+			echo "<br>";
+			echo "<br>";
+			$j = $j +1;
+			}
 			}
 ?>
 
@@ -28,7 +39,7 @@
     <title>Simple Polygon</title>
     <style>
       #map-canvas {
-        height: 600px;
+        height: 90%;
         margin: 0px;
         padding: 0px
       }
@@ -41,28 +52,29 @@ function initialize() {
   var mapOptions = {
     zoom: 5,
     <?php
+	echo "initialize";
+	//for($temp = 0; $temp < $j; $temp++){
 		$center = $Coords[sizeof($Coords)/2];
-    ?>
-    center: new google.maps.LatLng(<?=$center['lat']?>,<?=$center['lng']?>),
+		/* print_r($center[$temp]);
+		echo "<br>";
+		echo "<br>"; */
+		//}
+	?>
+	center: new google.maps.LatLng(<?=$center['lat']?>,<?=$center['lng']?>),
     mapTypeId: google.maps.MapTypeId.TERRAIN
+	}
   };
 
-  var Polygons = new array($size);
+  var Polygons = new Array(); 
   
-  <?php
-  echo "$size";
-  echo "<pre>";
-  echo "sizeof(Polygons)";
-  echo "</pre>";
-  ?>
-
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
   // Define the LatLng coordinates for the polygon's path.
-  var PolygonCoords = [
+  var PolygonCoords[] = [
     <?php
     array_shift($Coords);
+	//for($k = 0; $k < $j ;$k++){
     if(isset($Coords)){
         foreach($Coords as $c){
             $lat = $c['lat'];
@@ -74,25 +86,25 @@ function initialize() {
             echo "new google.maps.LatLng({$lat},{$lng}),\n";
         }
     }
+	//}
     ?>
   ];
-for($i = 1;$i <= sizeof(Polygons); $i++){
+  for(i = 0;i < sizeof(PolygonCoords); i++){
   // Construct the polygon.
-  Polygons[$i] = new google.maps.Polygon({
-    paths: PolygonCoords,
+  Polygons[] = new google.maps.Polygon({
+    paths: PolygonCoords[i],
     strokeColor: '#<?=random_color()?>',
     strokeOpacity: 0.8,
     strokeWeight: 2,
     fillColor: '#<?=random_color()?>',
     fillOpacity: 0.35
   });
-
-  Polygons[$i].setMap(map);
-  }
+  
+  Polygons[].setMap(map);
+  //} 
 }
-
+	
 google.maps.event.addDomListener(window, 'load', initialize);
-
     </script>
   </head>
   <body>
