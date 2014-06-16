4701be59defa765f5474925c61d6d8d79660c45c
damarneni <damarneni@cs2.mwsu.edu>
2014-06-16 13:54:25 -0500

    Updating PolygonOverlay

--- PolygonOverlay/functions.php
+++ PolygonOverlay/functions.php
@@ -1,24 +1,22 @@
 <?php
 function sql_to_coordinates($blob)
-    {
-        $blob = str_replace("))", "", str_replace("POLYGON((", "", $blob));
-        $coords = explode(",", $blob);
-        $coordinates = array();
-		//for($a = 0;$a <sizeof($coords);$a++){
-        foreach($coords as $coord)
-        {
-            $coord_split = explode(" ", $coord);
-            $coordinates[]=array("lat"=>$coord_split[0], "lng"=>$coord_split[1]);
-        }
-		//}
-        return $coordinates;
-    }
+   {
+       $blob = str_replace("))", "", str_replace("POLYGON((", "", $blob));
+       $coords = explode(",", $blob);
+       $coordinates = array();
+       foreach($coords as $coord)
+       {
+           $coord_split = explode(" ", $coord);
+           $coordinates[]=array("lat"=>$coord_split[0], "lng"=>$coord_split[1]);
+       }
+       return $coordinates;
+   }
 
-    function random_color_part() {
-        return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
-    }
+       function random_color_part() {
+               return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
+       }
 
-    function random_color() {
-        return random_color_part() . random_color_part() . random_color_part();
-    }
+       function random_color() {
+               return random_color_part() . random_color_part() . random_color_part();
+       }
 ?>
--- PolygonOverlay/index.php
+++ PolygonOverlay/index.php
@@ -3,127 +3,138 @@
 	include 'functions.php';
 	
 	unset($Coords);
-		if(isset($_POST['Countries'])){
-        $id = $_POST['Countries'];
-		//$size = sizeof($id);
-		/* echo "<pre>";
-		print_r($id);
-		echo "</pre>"; */
-		//$id = implode($id,",");
-		$j = 0;
-		foreach($id as $i){
-		$result = $con->query("SELECT asText(SHAPE) as border
-                               FROM `world_borders` 
-                               WHERE CountryID = '{$i}'");
-		
-		print_r($result);
-		echo "<br>";
-		echo "<br>";
-			$Result = $result->fetch_assoc();
-			//print_r($Result);
-            $Coords = sql_to_coordinates($Result['border']);
-			print_r($Coords);
-			echo "<br>";
-			echo "<br>";
-			echo "<br>";
-			echo "<br>";
-			$j = $j +1;
-			}
-			}
+
+       if(isset($_POST['Countries'])){
+               foreach($_POST['Countries'] as $Cid){
+                       $result = $con->query("SELECT asText(SHAPE) as border
+                                                                  FROM `world_borders`
+                                                                  WHERE CountryID = '{$Cid}'");
+
+                       $Result = $result->fetch_assoc();
+
+                       $CoordsArray[] = sql_to_coordinates($Result['border']);
+               }
+               $PrintMap = true;
+       }
+      /* if($_GET['debug']){
+               echo"<pre>";
+               print_r($CoordsArray);
+               exit;
+       } */ 
 ?>
-
+<!DOCTYPE html>
 <html>
-  <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <title>Simple Polygon</title>
-    <style>
-      #map-canvas {
-        height: 90%;
-        margin: 0px;
-        padding: 0px
-      }
-    </style>
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
-    <script>
-// This example creates a simple polygon representing the Bermuda Triangle.
+ <head>
+   <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+   <meta charset="utf-8">
+   <title>Simple Polygon</title>
+   <style>
+     #map-canvas {
+       height: 600px;
+       margin: 0px;
+       padding: 0px
+     }
+   </style>
+   <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
 
-function initialize() {
-  var mapOptions = {
-    zoom: 5,
-    <?php
-	echo "initialize";
-	//for($temp = 0; $temp < $j; $temp++){
-		$center = $Coords[sizeof($Coords)/2];
-		/* print_r($center[$temp]);
-		echo "<br>";
-		echo "<br>"; */
-		//}
-	?>
-	center: new google.maps.LatLng(<?=$center['lat']?>,<?=$center['lng']?>),
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-	}
-  };
+   <script>
+               // This example creates a simple polygon representing selected countries on a map.
 
-  var Polygons = new Array(); 
-  
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+               var i = 0;
 
-  // Define the LatLng coordinates for the polygon's path.
-  var PolygonCoords[] = [
-    <?php
-    array_shift($Coords);
-	//for($k = 0; $k < $j ;$k++){
-    if(isset($Coords)){
-        foreach($Coords as $c){
-            $lat = $c['lat'];
-            $lng = $c['lng'];
-            $lat = str_replace("(","",$lat);
-            $lng = str_replace("(","",$lng);
-            $lat = str_replace(")","",$lat);
-            $lng = str_replace(")","",$lng);
-            echo "new google.maps.LatLng({$lat},{$lng}),\n";
-        }
-    }
-	//}
-    ?>
-  ];
-  for(i = 0;i < sizeof(PolygonCoords); i++){
-  // Construct the polygon.
-  Polygons[] = new google.maps.Polygon({
-    paths: PolygonCoords[i],
-    strokeColor: '#<?=random_color()?>',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#<?=random_color()?>',
-    fillOpacity: 0.35
-  });
-  
-  Polygons[].setMap(map);
-  //} 
-}
-	
-google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
-  </head>
-  <body>
-    <div>
-        <form action="index.php" method="POST">
-            <?php
-            $result = $con->query("SELECT CountryID,name 
-                                   FROM `world_borders` 
-                                   ORDER BY name");
-            ?>
-            <select name="Countries[]" multiple>                   
-            <?php
-            while($row = $result->fetch_assoc()){
-                echo"<option value=\"{$row['CountryID']}\">{$row['name']}</option>";
-            }
-            ?>
-            <input type="submit" name="submit" value="Get Country">
-        </form>
-    </div>
-    <div id="map-canvas"></div>
-  </body>
+               function initialize() {
+                 var mapOptions = {
+                       zoom: 5,
+                       <?php
+                               $center = $CoordsArray[0][0];
+							    $lat = $center['lat'];
+                                $lng = $center['lng'];
+                                $lat = str_replace("MULTI","",$lat);
+                                $lng = str_replace("MULTI","",$lng);
+                                $lat = str_replace("MULTI","",$lat);
+                                $lng = str_replace("MULTI","",$lng);
+								$lat = str_replace("(","",$lat);
+                                $lng = str_replace("(","",$lng);
+                                $lat = str_replace(")","",$lat);
+                                $lng = str_replace(")","",$lng);
+                       ?>
+                       center: new google.maps.LatLng(<?=$lng?>,<?=$lat?>),
+                       mapTypeId: google.maps.MapTypeId.TERRAIN
+                 };
+
+                 var map = new google.maps.Map(document.getElementById('map-canvas'),
+                         mapOptions);
+
+                 var PolygonCoords = new Array();
+				 var PolyGon =new Array();
+
+                 <?php
+                 for($i=0;$i<sizeof($CoordsArray);$i++){
+
+                         echo"var Temp = [\n";
+                         echo"// Define the LatLng coordinates for the polygon's path.\n";
+                         array_shift($CoordsArray[$i]);
+                         $line=0;
+                         foreach($CoordsArray[$i] as $c){
+                                $lat = $c['lat'];
+                                $lng = $c['lng'];
+                                $lat = str_replace("(","",$lat);
+                                $lng = str_replace("(","",$lng);
+                                $lat = str_replace(")","",$lat);
+                                $lng = str_replace(")","",$lng);
+                                echo "new google.maps.LatLng({$lng},{$lat})";
+                                if($line < sizeof($CoordsArray[$i])-1){
+                                       echo ",\n";
+								}else{
+                                       echo "\n";
+                                }
+                                $line++;
+                         }
+                         echo"];\n";
+
+                         echo"PolygonCoords.push(Temp);\n";
+
+                         echo"// Construct the polygon.\n";
+                         echo"PolyGon[{$i}] = new google.maps.Polygon({\n";
+                                echo"paths: PolygonCoords[{$i}],\n";
+                                echo"strokeColor: '#FF0000',\n";
+                                echo"strokeOpacity: 0.8,\n";
+                                echo"strokeWeight: 2,\n";
+                                echo"fillColor: '#FF0000',\n";
+                                echo"fillOpacity: 0.35\n";
+                         echo"});\n";
+
+                         echo"PolyGon[{$i}].setMap(map);\n";
+                 }
+                 ?>
+
+               }
+
+               <?php
+               if($PrintMap){
+               echo"google.maps.event.addDomListener(window, 'load', initialize);\n";
+           }
+           ?>
+
+   </script>
+ </head>
+ <body>
+       <div>
+               <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+                       <?php
+                       $result = $con->query("SELECT CountryID,name
+                                                                  FROM `world_borders`
+                                                                  ORDER BY name");
+                       ?>
+       <select name="Countries[]"  multiple="multiple">
+                       <?php
+                       while($row = $result->fetch_assoc()){
+                               echo"\t\t<option value=\"{$row['CountryID']}\">{$row['name']}</option>\n";
+                       }
+                       ?>
+                       <input type="submit" name="submit" value="Get Country">
+               </form>
+       </div>
+   <div id="map-canvas"></div>
+ </body>
 </html>
